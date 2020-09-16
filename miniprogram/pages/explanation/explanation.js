f9c15167a2f2474c4d712b39534edd83f4001c32
yuyi.shi <yuyi.shi@scienjoy.com>
2020-09-16 19:02:08 +0800

    ts文件

--- miniprogram/pages/explanation/explanation.js
+++ miniprogram/pages/explanation/explanation.js
@@ -1,66 +1,7 @@
-// miniprogram/pages/explanation/explanation.js
+"use strict";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+    data: {},
+    onLoad: function () {
+    }
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhwbGFuYXRpb24uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJleHBsYW5hdGlvbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQ0EsSUFBSSxDQUFDO0lBQ0QsSUFBSSxFQUFDLEVBRUo7SUFDRCxNQUFNO0lBRU4sQ0FBQztDQUNKLENBQUMsQ0FBQSIsInNvdXJjZXNDb250ZW50IjpbIlxuUGFnZSh7XG4gICAgZGF0YTp7XG5cbiAgICB9LFxuICAgIG9uTG9hZCgpIHtcblxuICAgIH1cbn0pXG4iXX0=
